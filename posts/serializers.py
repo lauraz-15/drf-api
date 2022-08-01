4d7594e8933643a10b5d2a0ee52be77a276e1202
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-01 11:57:39 +0000

    Add post list view and set up permissions

--- posts/serializers.py
+++ posts/serializers.py
@@ -29,7 +29,7 @@ class PostSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     class Meta:
-        model = Profile
+        model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'title',
