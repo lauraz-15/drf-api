e72a184eb8f51eb8b213accd9f76b5ac6e4d118b
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-01 15:20:24 +0000

    Add likes functionality and list users likes

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List all likes
+    Like posts if authenticated
+    Associate the current logged in user with the like
+    """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """ Retrieve a like. No Update view, as users can
+    only like or unlike a post. Destroy a like, i.e. unlike a
+    post if owner of that like """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
